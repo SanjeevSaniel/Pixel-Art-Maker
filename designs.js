7b685bf03b16f1db4a0a0476a8a65511a92ca1aa
Sanjeev Saniel <sanjeevsaniel@outlook.in>
2020-07-20 11:03:20 +0530

    Minor improvements
    Fixed, the old grid was not replaced when submitting new grid.

--- designs.js
+++ designs.js
@@ -5,6 +5,8 @@ let size = document.getElementById("sizePicker");
 let pixels = document.getElementById("pixelCanvas");
 
 function makeGrid(height, width) {
+  // Remove the old grid
+  pixels.innerHTML = "";
   for (let x = 0; x < height; x++) {
     // Create rows based on height
     const row = document.createElement("tr");
@@ -20,21 +22,22 @@ function makeGrid(height, width) {
 
 // When size is submitted by the user, call makeGrid()
 
-function listener (event) {
-    event.preventDefault();
-    let inputHeight = document.getElementById("inputHeight").value;
-    let inputWidth = document.getElementById("inputWidth").value;
+function listener(event) {
+  event.preventDefault();
+  let inputHeight = document.getElementById("inputHeight").value;
+  let inputWidth = document.getElementById("inputWidth").value;
 
-    makeGrid(inputHeight, inputWidth);
+  makeGrid(inputHeight, inputWidth);
 }
 size.addEventListener("submit", listener);
 
-// Event listener for grid squares
-
-function pixelEvent (event) {
-    let pickedColor = color.value;
-    const clickedSquare = event.target;
-    // Change background color of clicked square
-    clickedSquare.style.backgroundColor = pickedColor;
+function pixelEvent(event) {
+  let pickedColor = color.value;
+  const clickedSquare = event.target;
+  // Change background color of clicked square
+  clickedSquare.style.backgroundColor = pickedColor;
 }
+// Event listener for grid squares
+// So that while scrolling down the page the color in the box
+// will not spread over
 pixels.addEventListener("mousedown", pixelEvent);
